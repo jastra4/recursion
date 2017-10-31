4c6de2e521eb663701db9a01f5c24f5a64110e5f
Joseph Strandmo <josephstrandmo@gmail.com>
2017-10-30 21:49:11 -0500

    Solved stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,57 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var jsonObj = ''
+  if (typeof obj === 'number') {
+    //jsonObj += parseInt(obj);
+    jsonObj += obj.toString();
+  } else if (typeof obj === 'string') {
+    jsonObj += "\"" + obj + "\"";
+  } else if (typeof obj === 'boolean') {
+    jsonObj += obj.toString();
+  } else if (Array.isArray(obj) === true) {
+  	if (obj.length === 0) {
+      return '[]';
+  	}
+  	jsonObj += '['
+    obj.forEach(function(element, index) {
+      if (index === obj.length - 1) {
+        jsonObj += stringifyJSON(element);
+      } else {
+      	jsonObj += stringifyJSON(element) + ',';
+      }
+    });
+    jsonObj += ']'
+  } else if (typeof obj === 'object') {
+  	if (Object.values(obj).length === 0) {
+      return '{}';
+  	}
+  	jsonObj += '{';
+    for (var key in obj) {
+      if (typeof obj[key] === 'undefined' || typeof obj[key] === 'function') { //should skip functions and undefined keys
+        //skip
+      } else {
+      if (obj[key] === null) {
+      	jsonObj += stringifyJSON(key);
+        jsonObj += ':';
+        jsonObj += 'null';
+      } else {
+      	if (stringifyJSON(key) === stringifyJSON(Object.keys(obj)[Object.keys(obj).length-1])) { //check if key is last in object
+          jsonObj += stringifyJSON(key);
+          jsonObj += ':';
+          jsonObj += stringifyJSON(obj[key]);
+      	} else {
+          jsonObj += stringifyJSON(key);
+          jsonObj += ':';
+          jsonObj += stringifyJSON(obj[key]) + ',';
+      	}
+      }
+      }
+
+    }
+    jsonObj += '}';
+  } else {
+  	jsonObj += null;
+  }
+  return jsonObj;
 };
